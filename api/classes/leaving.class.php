abb0632be7b4d701e079bf53d0480afd4e6667e2
Per Olsson <per.olsson@gmail.com>
2017-07-16 07:40:14 +0200

    Analytics tracking for leaving or passing site

--- api/classes/leaving.class.php
+++ api/classes/leaving.class.php
@@ -5,6 +5,20 @@
 		static public function fallback($data) {
 
 			$url = $data['for'];
+			$referrer = isset($data['referrer']) ? $data['referrer'] : 'Unknown';
+
+			$payload = [
+				'v' 	=> '1', 				// Version.
+				'tid' 	=> 'UA-99821474-2', 	// Tracking ID / Property ID.
+				'cid' 	=> '777',				// Anonymous Client ID.
+				't' 	=> 'event',				// Hit Type. pageview/event
+				'ec' 	=> 'Leaving',	        // Event Category. Required.
+				'ea' 	=> $referrer,         	// Event Action. Required.
+				'el' 	=> $url,      			// Event label.
+			];
+
+			$response = Curl::get('https://www.google-analytics.com/collect', false, 'post', $payload);
+
 			header('Location: '.$url);
 
 		}
