cbb6dd2f8431c97d64481f127a7a28f5462b5929
Per Olsson <per.olsson@gmail.com>
2017-07-23 18:02:14 +0200

    Fix to corrupt blog title

--- api/classes/content.class.php
+++ api/classes/content.class.php
@@ -74,10 +74,11 @@
 			$str = strip_tags($str); 
 			$str = preg_replace('/[\r\n\t ]+/', ' ', $str);
 			$str = preg_replace('/[\"\*\/\:\<\>\?\'\|]+/', ' ', $str);
+			$str = str_replace('[', '', $str);
+			$str = str_replace(']', '', $str);
 			$str = html_entity_decode( $str, ENT_QUOTES, "utf-8" );
 			$str = htmlentities($str, ENT_QUOTES, "utf-8");
 
-			var_dump($str);
 			return $str;
 
 		}
